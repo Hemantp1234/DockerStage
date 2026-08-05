03dce972f485ce1a76b18dde70028d71c8d2f3fd
Hemant Patil <hemantpatil@MacBook-Pro.local>
2026-08-05 22:02:16 +0530

    single and multi stage

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+      fontFamily: 'sans-serif',
+      backgroundColor: '#0f172a',
+      color: '#38bdf8'
+    }}>
+      <h1>Hi Hemant 👋</h1>
+    </div>
+  )
+}
+
+export default App
